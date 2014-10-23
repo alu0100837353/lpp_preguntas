faf986dafbd657c0a78e1406b1b5b89bb4e194e8
Victor Hernandez Perez <alu0100697032.edu.es>
2014-10-23 13:49:55 +0000

    Arregladas las espectativas: deben existir respuestas y una pregunta

--- spec/examen_spec.rb
+++ spec/examen_spec.rb
@@ -2,14 +2,18 @@ require 'examen'
 
 describe Pregunta do
   before :each do
-    @pregunta = Pregunta.new("¿Que?", "a", "b", "c", "d")
+    @p1 = Pregunta.new("Enunciado", "a", "b", "c", "d")
   end
   
   describe "La pregunta es coherente" do 
       it "Existe enunciado" do
-          pregunta.respuesta_a.should == "a"
+          @p1.enunciado != nil
       end
-      #it "Existen respuestas" do 
-      #end
+      it "Existen respuestas" do 
+        @p1.respuesta_a != nil
+        @p1.respuesta_b != nil
+        @p1.respuesta_c != nil
+        @p1.respuesta_d != nil
+      end
   end
 end
