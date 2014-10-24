f05b4feb873480af9fd0b0fc8c2b6587a8baa602
alu0100837353 <alu0100837353@ull.edu.es>
2014-10-24 09:58:36 +0000

    Cambios en el Rakefile y en exam_spec.rb

--- Rakefile
+++ Rakefile
@@ -8,9 +8,10 @@ end
 desc "Ejecutar espectativas de la clase Pregunta"
 task :spec do 
 #run test with --format documentation 
-    sh "rspec -Ilib -Ispec spec/examen_spec.rb"
+    sh "rspec -I lib -Ispec spec/examen_spec.rb --format documentation"
 end
 
 task :thtml do 
 #run test with format: html
+     sh "rspec -I lib -Ispec spec/examen_spec.rb --format html"
 end
--- spec/examen_spec.rb
+++ spec/examen_spec.rb
@@ -16,4 +16,26 @@ describe Pregunta do
         @p1.respuesta_d != nil
       end
   end
+  
+  describe "Getter de enunciado" do
+      it "getenum devuelve el valor de p1.enunciado" do
+        @p1.getenun.should eq ("Enunciado")
+      end
+  end
+  
+  describe "Getter de respuestas" do  
+      it "getres_i devuelve el valor de p1.respuesta_i" do
+        @p1.getres_a.should eq ("a")
+        @p1.getres_b.should eq ("b")
+        @p1.getres_c.should eq ("c")
+        @p1.getres_d.should eq ("d")
+      end
+  end
+  
+  describe "to_s" do
+      it "to_s devuelve una cadena con la preguntas y las respuestas para poder mostrar por consola" do
+        @p1.to_s.should eq ("Enunciado \n a) a \n b) b \n c) c \n d d ")
+      end
+  end
+    
 end
